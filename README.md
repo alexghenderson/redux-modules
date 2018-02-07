b53b4e08d1454f5c1da61694429e2ff714c91581
Alex Henderson <alex@alexhenderson.me>
2018-02-06 21:01:24 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,110 @@
+## Overview
+Heavily inspired by redux-box, some of the ideas and methodology is shamelessly ripped from it. However, redux-modules allows easier integration into existing code. Unlike redux-box, it does not require a custom store creator and does not provide sagas (although this may be added in the future).
+
+## Installation
+Install react, redux, and react-redux:
+```bash
+npm install --save react redux react-redux
+```
+
+Install redux-modules:
+```bash
+npm install --save @alexghenderson/redux-modules
+```
+
+## Usage
+redux-modules provides a method of declaring redux modules, allowing you to easily integrate them into your existing store and mapping them to your component, either through higher order components or child functions
+
+`src/store/user/index.js`
+```js
+import {createTypes, createAction, createContainer} from '@alexghenderson/redux-modules';
+
+const name = 'users'
+
+const types = createTypes(name)({
+    ADD_USER: 'ADD_USER'
+});
+
+const initial = [];
+
+const mutations = {
+    [types.ADD_USER]: (state, {payload}) => (state.push(payload)),
+};
+
+const actions = {
+    addUser: createAction(types.ADD_USER),
+};
+
+export const module = {
+    name,
+    initial,
+    mutations,
+    actions,
+};
+
+export default createContainer(module);
+```
+`src/store/reducers.js`
+```js
+import {module as userModule} from './user';
+
+const moduleReducers = getReducers([
+    navModule,
+]);
+
+const reducers = {
+    //other reducers,
+    ...moduleReducers,
+};
+
+export default reducers;
+```
+
+`src/store/index.js`
+```js
+import {createStore} from 'redux';
+
+import reducers from './reducers';
+
+export default () => {
+    const store = createStore(reducers, {});
+
+    return store;
+};
+```
+
+Any component file (using connect)
+```js
+import {connect} from '@alexghenderson/redux-modules';
+import {module as userModule} from 'src/store/user';
+
+@connect({
+  user: userModule,
+})
+class SomeComponent extends React.Component {
+  render() {
+    const {user} = this.props;
+    return (
+      //component code
+    )
+  }
+}
+```
+
+or, using child functions
+```js
+import UserModule from 'src/store/user';
+
+const SomeComponent = () => (
+  <UserModule>
+    {(users)=>(
+
+    )}
+  </UserModule>
+)
+
+
+See [sample](https://github.com/alexghenderson/formik-field/blob/master/sample/src/App.js) for format and normalize
+
+## Tips
+Make a snippet for this. 
---
+++ src/index.js
@@ -0,0 +1,85 @@
+import {connect as c} from 'react-redux';
+import produce from 'immer';
+
+const createReducer = (module) => (state = module.initial, action) => (
+    module.mutations[action.type] 
+    ? produce(state, (draft) => (module.mutations[action.type](draft, action))) 
+    : state
+)
+
+const getActions = (module) => (module.actions);
+
+const selectModule = (module) => (state) => (state[module.name]);
+
+const dispatchModule = (module, dispatch) => (
+    Object.keys(module.actions).reduce(
+        (acc, key)=>({
+            ...acc,
+            [key]: (...args) => (dispatch(module.actions[key](...args))),
+        }), {}
+    )
+);
+
+export const getReducers = (modules) => (modules.reduce(
+        (acc, module)=>({
+            ...acc,
+            [module.name]: createReducer(module),
+        }), {}
+    )
+);
+
+export const connectContainer = (module) => (Component) => {
+    console.log(module);
+    return c(
+        selectModule(module),
+        (dispatch) => dispatchModule(module, dispatch),
+    )(Component)
+}
+
+const Container = ({children, ...props}) => (children(props));
+
+export const createContainer = (module) => connectContainer(module)(Container)
+
+export const connect = (modules) => (
+    c(
+        (state)=>( //mapStateToProps
+            Object.keys(modules).reduce(
+                (acc, key)=>({
+                    ...acc,
+                    [key]: selectModule(modules[key])(state),
+                }), {}
+            ) 
+        ),
+        (dispatch)=>( //mapDispatchToProps
+            Object.keys(modules).reduce(
+                (acc, key)=>({
+                    ...acc,
+                    [key]: dispatchModule(modules[key], dispatch)
+                }), {}
+            ) 
+        ),
+        (state, actions, own)=>( //mergeProps
+            Object.keys(modules).reduce(
+                (acc, key)=>({
+                    ...acc,
+                    [key]: {
+                        ...state[key],
+                        ...actions[key],
+                        ...own[key],
+                    }
+                }), {...own}
+            )
+        )
+    )
+)
+
+export const createTypes = (name) => (types) => (Object.keys(types).reduce(
+    (acc, type) => ({
+        ...acc,
+        [type]: `${name.toUpperCase()}/${types[type]}`,
+    }), {}
+));
+
+export const createAction = (type, pc) => (props) => ({type, payload: pc && pc.call ? pc(props) : pc})
+
+export const identity = (i) => (i);
